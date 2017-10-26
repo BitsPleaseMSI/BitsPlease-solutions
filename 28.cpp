a974ce8be4c32e865a63f75b5d75b6cb42744ab8
Amit Bansal <007amitbansal@gmail.com>
2017-10-26 18:36:27 +0530

    28.Find The Last Digit

--- 28.cpp
+++ 28.cpp
@@ -12,13 +12,13 @@ int main()
 	 * temp is used for processing of data
 	*/
 	int n, *arr, a, b, temp;
-	cin>>n;
+	cin >> n;
 	arr = new int[n];
-	for(int i=0; i<n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		cin>>a>>b;
-		temp=1;
-		while(b>0)
+		cin >> a >> b;
+		temp = 1;
+		while (b > 0)
 		{
 			temp = temp * a;
 			temp = temp % 10;		// Since we only need the last digit
@@ -26,9 +26,9 @@ int main()
 		}
 		arr[i] = temp;
 	}
-	cout<<"\n\n";
-	for(int i=0; i<n; i++)
-		cout<<"\n"<<arr[i];
+	cout << "\n\n";
+	for (int i = 0; i < n; i++)
+		cout << "\n" << arr[i];
 	getch();
 	delete[] arr;
 	return 0;

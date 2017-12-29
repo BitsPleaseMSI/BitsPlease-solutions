c1f4fda628f59e3f9d483e86c0209833e461acb7
Good-Liver <34918834+Good-Liver@users.noreply.github.com>
2017-12-29 22:05:29 +0530

    Add files via upload

--- 10.cpp
+++ 10.cpp
@@ -1,84 +1,19 @@
-#include<iostream>
+#include <iostream>
+#include <math.h> 
 using namespace std;
-int main()
-{
-	int n, m, i, a[30], b[30], sum[30], x, l;
 
-	//initialising sum array as 0
-	for (i = 0; i < 30; i++)
-	{
-		sum[i] = 0;
-		a[i] = 0;
-		b[i] = 0;
+int main() {
+	int n,m,i,sum,N[1000000],M[1000000];
+	cin>>n;
+	for(i=0;i<n;i++)
+	{   cin>>N[i];
+	    sum+=(pow(10,n-1-i))*N[i];
 	}
-
-	//input array 1
-	cin >> n;
-	for (i = 0; i < n; i++)
-		cin >> a[i];
-
-	//input array 2
-	cin >> m;
-	for (i = 0; i < m; i++)
-		cin >> b[i];
-
-	//array resizing
-	if (n > m)
-	{
-		l = n - m;
-		for (i = m; i >= 0; i--)
-			b[i + l] = b[i];
-		for (i = 0; i < l; i++)
-			b[i] = 0;
+	cin>>m;
+	for(i=0;i<m;i++)
+	{   cin>>M[i];
+	    sum+=(pow(10,m-1-i))*M[i];
 	}
-
-	if (m > n)
-	{
-		l = m - n;
-		for (i = n; i >= 0; i--)
-			a[i + l] = a[i];
-		for (i = 0; i < l; i++)
-			a[i] = 0;
-	}
-
-	x = n > m ? n : m;
-
-	//adding the array
-	for (i = x; i >= 0; i--)
-	{
-	//case 1: sum<10
-		if (a[i] + b[i] < 10)
-			sum[i] += a[i] + b[i];
-	//case 2: sum>=10, i!=0
-		else if (a[i] + b[i] >= 10)
-		{
-			sum[i - 1] += 1;
-			sum[i] += (a[i] + b[i]) - 10;
-		}
-	//case 3: sum>=10, i=0
-		if (i == 0 && (a[i] + b[i] >= 10))
-		{
-			for (i = x; i >= 0; i--)
-				sum[i + 1] = sum[i];
-			sum[0] = 1;
-			x += 1;
-		}
-	}
-
-	//display
-	for (i = 0; i < x; i++)
-		cout << sum[i];
-
+	cout<<sum;
 	return 0;
 }
-
-
-
-    
-    
-    
-    
-    
-    
-    
-    
---
+++ 3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h> 
+using namespace std;
+
+int main() {
+	int tc;
+	cin>>tc;
+	while(tc--)
+	{   int n,i,j,N[100000],var,sum=0,k,l;
+	    cin>>n;
+	    for(i=0;i<n;i++)
+	    {
+	        cin>>N[i];
+	    }
+	    var=-1000000;
+	    for(i=1;i<=n;i++)
+	    {
+	        for(j=0;j<=n-i;j++)
+	        {
+	            k=i+j-1;
+	            for(l=j;l<=k;l++)
+	            {   
+	                sum+=N[l];
+	            }
+	            if(var<sum)
+	            {
+	                var=sum;
+	            }
+	            sum=0;
+	        }
+	        
+	    }
+	    cout<<var<<endl;
+	}
+}

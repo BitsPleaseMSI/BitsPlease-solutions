03198fda8b29b41752ece591bf0b7ee23eb3c0f0
Pranav Khurana <apranavkhurana18@gmail.com>
2018-01-26 02:16:10 +0530

    added 21.cpp(microwave tower

---
+++ 21.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t_cases,n,i,j,l_tow[10],sig;
+    cin>>t_cases;
+    while(t_cases)
+    {
+                  cin>>n;
+                  for(i=0;i<n;i++)
+                  cin>>l_tow[i];
+                  for(i=0;i<n;i++)
+                  {
+                                     j=i-2;
+                                     sig=1;
+                                     if(i==0)
+                                     sig=0;
+                                     while((l_tow[j]<=l_tow[i])&&j>=0)
+                                     {
+                                                              j--;
+                                                              sig++;
+                                     }
+                                     cout<<sig<<" ";
+                                                              
+                  }
+                  cout<<"\n";
+                  t_cases--;
+    }
+    return 0;
+}
+                                        

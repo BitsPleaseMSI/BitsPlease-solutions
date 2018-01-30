7d376fcdfbed8ff90e28d82e48210f6927e78706
Sreejeet <31929912+sreejeet@users.noreply.github.com>
2018-01-30 21:56:02 +0530

    Removed unnecessary comments.

--- 4.c
+++ 4.c
@@ -5,13 +5,14 @@ int main(void) {
 	char A[20];
 	int i, j, k, t, count=0;
 	scanf("%d", &t);
-	for(i=0; i<t; i++) { //loop 1
+	for(i=0; i<t; i++){
 		scanf("%s", &A);
-		for(j=0; j<strlen(A); j++) { //loop 2 from 0 index of string till strlen
-			if(A[j]=='1') { //checks for '1'
-				if(A[j+1]=='1') { //checks for adjacent '1'
-					for(k=j; A[k]!='0'; k++); //moves index to '0'
-					j=k; count++; //count incremented, index skipped
+		for(j=0; j<strlen(A); j++){
+			if(A[j]=='1'){
+				if(A[j+1]=='1'){
+					for(k=j; A[k]!='0'; k++);
+					j=k;
+					count++;
 				}
 			}
 		}

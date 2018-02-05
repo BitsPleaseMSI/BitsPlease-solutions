23057d81b67d9d60c4cc8cc6482976a5ae692d90
Rahul puri <34956557+purirahul@users.noreply.github.com>
2018-02-05 22:51:29 +0530

    Update Q19.cpp

--- Q19.cpp
+++ Q19.cpp
@@ -5,40 +5,46 @@ int main()
 {
 	int ch, a[9];
 	int f=0 ,g=0 ,h=0, c, s=0,r,i;
+	int n;
+	cin>>n;
+	while(n--)
+	{
+		 f=0 ;g=0 ;h=0; s=0;
+		cin>>ch;
+		if(ch%9==0)          // checking 1st condition
+        	f =1;
 
-	cin>>ch;
-	if(ch%9==0)          // checking 1st condition
-        f =1;
+       		i = 0;
+   	 while(ch>0)         // checking 3rd condition
+    	{
+       	 	r = ch%10;
+        	s += r;
+      	  	ch = ch/10;
+       		 a[i] = r;
+       	 	i++;
+    	}
+		if(s >=40)
+       		 h =1;
 
-       i = 0;
-    while(ch>0)         // checking 3rd condition
-    {
-        r = ch%10;
-        s += r;
-        ch = ch/10;
-        a[i] = r;
-        i++;
-    }
-	if(s >=40)
-        h =1;
+   	 for(int i= 0; i<9 ;i++)     // checking 2nd condition
+    	{
+       		 c = 0;
+        	for(int j=0;j<9; j++)
+        	{
+            		if(a[i] == a[j])
+                	c= c +1;
 
-    for(int i= 0; i<9 ;i++)     // checking 2nd condition
-    {
-        c = 0;
-        for(int j=0;j<9; j++)
-        {
-            if(a[i] == a[j])
-                c= c +1;
-
-        }
-        if(c ==3)
-            g =1;
-    }
+        	}
+        	if(c ==3)
+           	 g =1;
+  	  }
 
     if(f==1 && g == 1 && h==1)
-        cout<<"Original";
+        cout<<"Original"<<"\n";
 
     else
-        cout<<"fake";
- }
+        cout<<"Fake"<<"\n";
+	 }
+}
+
 

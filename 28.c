a020c7e03c8477d438bcf80e3b1a238bce83ee74
Animesh-Ghosh <34956994+Animesh-Ghosh@users.noreply.github.com>
2018-03-03 19:49:01 +0530

    Update 28.c
    Added checks for a and b. 
    Added while loop for multiple input lines.

--- 28.c
+++ 28.c
@@ -1,78 +1,69 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void) {
-	int a, b, n, d, l;
-	scanf("%d %d", &a, &b);
-	d = a%10;
-	switch(d) {
-		case 0: if(b==0) printf("Undefined.\n");
+	int N, a, b, n, d, l;
+	scanf("%d", &N);
+	while(N>0) {
+		scanf("%d %d", &a, &b);
+		if(a>=0 && a<=30 && b>=0 && b<=pow(10, 12)) {
+			d = a%10;
+			switch(d) {
+			case 0: if(b==0) printf("Undefined.\n");
 				else printf("0\n");
 				break;
-		case 1: printf("1\n");
-				break;
-		case 2: l = b%4;
+			case 1: printf("1\n"); break;
+			case 2: l = b%4;
 				switch(l) {
-					case 1: printf("2\n");
-							break;
-					case 2: printf("4\n");
-							break;
-					case 3: printf("8\n");
-							break;
-					default: printf("6\n");
+				case 1: printf("2\n"); break;
+				case 2: printf("4\n"); break;
+				case 3: printf("8\n"); break;
+				default: printf("6\n");
 				}
 				break;
-		case 3: l = b%4;
+			case 3: l = b%4;
 				switch(l) {
-					case 1: printf("3\n");
-							break;
-					case 2: printf("9\n");
-							break;
-					case 3: printf("7\n");
-							break;
-					default: printf("1\n");
+				case 1: printf("3\n"); break;
+				case 2: printf("9\n"); break;
+				case 3: printf("7\n"); break;
+				default: printf("1\n");
 				}
 				break;
-		case 4: l = b%2;
+			case 4: l = b%2;
 				switch(l) {
-					case 1: printf("4\n");
-							break;
-					default: printf("6\n");
+				case 1: printf("4\n"); break;
+				default: printf("6\n");
 				}
 				break;
-		case 5: printf("5\n");
-				break;
-		case 6: printf("6\n");
-				break;
-		case 7: l = b%4;
+			case 5: printf("5\n"); break;
+			case 6: printf("6\n"); break;
+			case 7: l = b%4;
 				switch(l) {
-					case 1: printf("7\n");
-							break;
-					case 2: printf("9\n");
-							break;
-					case 3: printf("3\n");
-							break;
-					default: printf("1\n");
+				case 1: printf("7\n"); break;
+				case 2: printf("9\n"); break;
+				case 3: printf("3\n"); break;
+				default: printf("1\n");
 				}
 				break;
-		case 8: l = b%4;
+			case 8: l = b%4;
 				switch(l) {
-					case 1: printf("8\n");
-							break;
-					case 2: printf("4\n");
-							break;
-					case 3: printf("2\n");
-							break;
-					default: printf("6\n");
+				case 1: printf("8\n"); break;
+				case 2: printf("4\n"); break;
+				case 3: printf("2\n"); break;
+				default: printf("6\n");
 				}
 				break;
-		case 9: l = b%2;
+			case 9: l = b%2;
 				switch(l) {
-					case 1: printf("9\n");
-							break;
-					default: printf("1\n");
+				case 1: printf("9\n"); break;
+				default: printf("1\n");
 				}
 				break;
-		default: printf("Error.\n");
+			default: printf("Error.\n");
+			}
+		}
+		else printf("a or b not in range.\n");
+		N--;
 	}
 	return 0;
 }

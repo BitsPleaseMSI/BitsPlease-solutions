4c3585dba39cc49cbdd631084753527eab6656e2
Animesh-Ghosh <34956994+Animesh-Ghosh@users.noreply.github.com>
2018-06-06 17:31:36 +0530

    Update 35.c
    Changed a few variable names, changed some formatting.

--- 35.c
+++ 35.c
@@ -1,67 +1,64 @@
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>
-#define N 3 //size of matrix can be changed from here
+#define NUM 3 //size of matrix can be changed from here
 
-void EnterM(int A[][N], int);
-void ShowM(int A[][N], int);
-int detA(int A[][N], int);
-void Minor(int A[][N], int M[][N], int, int, int);
+void Enter(int A[][NUM], int);
+void Show(int A[][NUM], int);
+int detA(int A[][NUM], int);
+void Minor(int A[][NUM], int M[][NUM], int, int, int);
 
 void main() {
-	int A[N][N], det;
+	int A[NUM][NUM], det;
 	printf("Enter values in matrix:-\n");
-	EnterM(A, N);
+	Enter(A, NUM);
 	printf("\n");
-	ShowM(A, N);
-	printf("Evaluating determinant:-\n");
-	printf("\n");
-	det = detA(A, N);
+	Show(A, NUM);
+	printf("\nEvaluating determinant:-\n");
+	det = detA(A, NUM);
 	printf("Determinant of entered matrix = %d", det);
 	getch();
 }
 
-void EnterM(int A[][N], int n) {
+void Enter(int A[][NUM], int num) {
 	int i, j, k=0;
-	for (i=0; i<n; i++) {
-		for (j=0; j<n; j++, k++) {
+	for (i=0; i<num; i++) {
+		for (j=0; j<num; j++, k++) {
 			printf("Enter element %d: ", (k+1));
 			scanf("%d", &A[i][j]);
 		}
 	}
 }
 
-void ShowM(int A[][N], int n) {
+void Show(int A[][NUM], int num) {
 	int i, j;
 	printf("Matrix elements:-\n");
-	for (i=0; i<n; i++) {
-		for (j=0; j<n; j++) 
+	for (i=0; i<num; i++) {
+		for (j=0; j<num; j++) 
 			printf("%d ", A[i][j]);
 		printf("\n");
 	}
 }
 
 
-int detA(int A[][N], int n) {
+int detA(int A[][NUM], int num) {
 	int det = 0, i;
-	int M[n-1][n-1];
-	if (n==1) return A[0][0];
+	int M[num-1][num-1];
+	if (num==1) return A[0][0];
 	else {
-		for (i=0; i<n; i++) {
-			Minor(A, M, 0, i, n);
-			ShowM(M, n-1);
-			det = det + (pow(-1, i) * A[0][i] * detA(M, n-1));
-			printf("det = %d\n", det);
+		for (i=0; i<num; i++) {
+			Minor(A, M, 0, i, num);
+			det = det + (pow(-1, i) * A[0][i] * detA(M, num-1));
 		}
 		return det;
 	}
 }
 
-void Minor(int A[][N], int M[][N], int I, int J, int n) {
+void Minor(int A[][NUM], int M[][NUM], int I, int J, int num) {
 	int i, j, k=0, l=0;
-	for (i=0; i<n; i++) {
+	for (i=0; i<num; i++) {
 		if (i!=I) {
-			for (j=0; j<n; j++) {
+			for (j=0; j<num; j++) {
 				if (j!=J) {
 					M[k][l] = A[i][j];
 					l++;

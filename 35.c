9bae4b84d8548c1a1394dd0cc03743d010dc6f8e
Animesh-Ghosh <34956994+Animesh-Ghosh@users.noreply.github.com>
2018-06-06 18:42:09 +0530

    Update 35.c
    Changed output format to match the requirements.

--- 35.c
+++ 35.c
@@ -1,64 +1,49 @@
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>
-#define NUM 3 //size of matrix can be changed from here
+#define MAXSIZE 5  //size of matrix can be changed from here
 
-void Enter(int A[][NUM], int);
-void Show(int A[][NUM], int);
-int detA(int A[][NUM], int);
-void Minor(int A[][NUM], int M[][NUM], int, int, int);
+void Enter(int A[][MAXSIZE], int);
+void Show(int A[][MAXSIZE], int);
+int detA(int A[][MAXSIZE], int);
+void Minor(int A[][MAXSIZE], int M[][MAXSIZE], int, int, int);
 
 void main() {
-	int A[NUM][NUM], det;
-	printf("Enter values in matrix:-\n");
-	Enter(A, NUM);
-	printf("\n");
-	Show(A, NUM);
-	printf("\nEvaluating determinant:-\n");
-	det = detA(A, NUM);
-	printf("Determinant of entered matrix = %d", det);
+	int A[MAXSIZE][MAXSIZE], size, det;
+	scanf("%d", &size);
+	Enter(A, size);
+	det = detA(A, size);
+	printf("%d", det);
 	getch();
 }
 
-void Enter(int A[][NUM], int num) {
-	int i, j, k=0;
-	for (i=0; i<num; i++) {
-		for (j=0; j<num; j++, k++) {
-			printf("Enter element %d: ", (k+1));
+void Enter(int A[][MAXSIZE], int size) {
+	int i, j;
+	for (i=0; i<size; i++) {
+		for (j=0; j<size; j++) {
 			scanf("%d", &A[i][j]);
 		}
 	}
 }
 
-void Show(int A[][NUM], int num) {
-	int i, j;
-	printf("Matrix elements:-\n");
-	for (i=0; i<num; i++) {
-		for (j=0; j<num; j++) 
-			printf("%d ", A[i][j]);
-		printf("\n");
-	}
-}
-
-
-int detA(int A[][NUM], int num) {
+int detA(int A[][MAXSIZE], int size) {
 	int det = 0, i;
-	int M[num-1][num-1];
-	if (num==1) return A[0][0];
+	int M[size-1][size-1];
+	if (size==1) return A[0][0];
 	else {
-		for (i=0; i<num; i++) {
-			Minor(A, M, 0, i, num);
-			det = det + (pow(-1, i) * A[0][i] * detA(M, num-1));
+		for (i=0; i<size; i++) {
+			Minor(A, M, 0, i, size);
+			det = det + (pow(-1, i) * A[0][i] * detA(M, size-1));
 		}
 		return det;
 	}
 }
 
-void Minor(int A[][NUM], int M[][NUM], int I, int J, int num) {
+void Minor(int A[][MAXSIZE], int M[][MAXSIZE], int I, int J, int size) {
 	int i, j, k=0, l=0;
-	for (i=0; i<num; i++) {
+	for (i=0; i<size; i++) {
 		if (i!=I) {
-			for (j=0; j<num; j++) {
+			for (j=0; j<size; j++) {
 				if (j!=J) {
 					M[k][l] = A[i][j];
 					l++;

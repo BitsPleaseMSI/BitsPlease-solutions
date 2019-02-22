0649d24bc806a4bcdb5b799972e6ef493620b02d
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-02-22 22:52:11 +0530

    Update 19.cpp
    Implementing use of test cases to conform to the input format given in http://bitsplease.pythonanywhere.com/problem/19.

--- 19.cpp
+++ 19.cpp
@@ -1,47 +1,35 @@
-#include<iostream>
-#include<conio.h>
+#include <iostream>
 
 using namespace std;
 
-int main()
-{
-	/*
-	 * Since its hard to process a single integer value.
-	 * So, we input serial number as string and convert
-	 * it as array of integers
-	*/
-	char serial_num[9];
-	int serial[9], test, flag=0, sum=0;
-	gets(serial_num);
-	for(int i=0; i<9; i++)
-	{
-		serial[i] = (int)serial_num[i]-48;
-	}
-		
-	// To check if a digit repeats itself 3 times.
-	for(int j=0; j<7; j++)
-	{
+int CheckRepeat (int serial[9]) {
+	int test, flag = 0;
+	for (int j = 0; j < 7; j++) {
 		test = serial[j];
-		for(int i=0; i<9; i++)
-		{
-			if(serial[i]==test)
-			{
-				if(serial[i+1]==test && serial[i+2]==test)
+		for(int i = 0; i < 9; i++) {
+			if(serial[i] == test) {
+				if(serial[i + 1] == test && serial[i + 2] == test)
 					flag = 1;
 			}
 		}
 	}
-	
-	// To do divisibility test and check sum
-	for(int i=0; i<9; i++)
-	{
-		sum = sum + serial[i];
-	}
-	if(flag==1 && sum>40 && sum%9==0)
-		cout<<"\nOriginal";
-	else
-		cout<<"\nFake";
-	getch();
-	return 0;
+	return flag;
 }
 
+int main () {
+	char serial_num[9];
+	int serial[9], t, sum;
+	cin >> t;
+	while (t--) {
+		sum = 0;
+		cin >> serial_num;
+		for (int i = 0; i < 9; i++)
+			serial[i] = (int) serial_num[i] - 48;
+		for(int i = 0; i < 9; i++) 
+			sum = sum + serial[i];
+		if(CheckRepeat (serial) && sum > 40 && sum % 9 ==0)
+			cout << "Original" << endl;
+		else cout << "Fake" << endl;
+	}
+	return 0;
+}

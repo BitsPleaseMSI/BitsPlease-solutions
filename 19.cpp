d8901856dbf6b5952a20a257e8d63626a95ebb1f
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-02-23 20:42:08 +0530

    Update 19.cpp
    Correcting output format.

--- 19.cpp
+++ 19.cpp
@@ -6,9 +6,9 @@ int CheckRepeat (int serial[9]) {
 	int test, flag = 0;
 	for (int j = 0; j < 7; j++) {
 		test = serial[j];
-		for(int i = 0; i < 9; i++) {
-			if(serial[i] == test) {
-				if(serial[i + 1] == test && serial[i + 2] == test)
+		for (int i = 0; i < 9; i++) {
+			if (serial[i] == test) {
+				if (serial[i + 1] == test && serial[i + 2] == test)
 					flag = 1;
 			}
 		}
@@ -17,19 +17,23 @@ int CheckRepeat (int serial[9]) {
 }
 
 int main () {
-	char serial_num[9];
+	char **serial_num;
 	int serial[9], t, sum;
 	cin >> t;
-	while (t--) {
-		sum = 0;
-		cin >> serial_num;
-		for (int i = 0; i < 9; i++)
-			serial[i] = (int) serial_num[i] - 48;
-		for(int i = 0; i < 9; i++) 
+    	serial_num = new char*[t];
+    	for (int i = 0; i < t; i++) {
+        	serial_num[i] = new char[9];
+        	cin >> serial_num[i];
+    	}
+    	for (int i = 0; i < t; i++) {
+        	sum = 0;
+        	for (int j = 0; j < 9; j++)
+            		serial[j] = (int) serial_num[i][j] - 48;
+        	for (int i = 0; i < 9; i++) 
 			sum = sum + serial[i];
-		if(CheckRepeat (serial) && sum > 40 && sum % 9 ==0)
+		if (CheckRepeat (serial) && sum > 40 && sum % 9 ==0)
 			cout << "Original" << endl;
 		else cout << "Fake" << endl;
-	}
+    	}
 	return 0;
 }

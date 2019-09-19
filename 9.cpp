74ecb2b916ef3f607645ca376a42df4f0e40b6a7
Tushar Bali <tusharbali11@gmail.com>
2019-09-19 23:03:57 +0530

    Create 9.cpp

---
+++ 9.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    cout<<"Enter your commands(<,>,V,^) without spaces"<<endl;
+    string directions;
+    int str_size,msize,mid_x,mid_y,x=0,y=0,total=0;
+    getline(cin,directions);
+    str_size=directions.length();
+    msize=(2*str_size)+1;
+    int gmap[msize][msize]={};      //to initialize array with all elements=0
+
+    mid_x=msize/2;
+    mid_y=mid_x;
+    gmap[mid_x][mid_y]={1};           //since starting position is
+                                      //considered a house
+    for(int i=0;i<msize;i++)
+    {
+        if(directions[i]=='<')
+        y++;
+        else if(directions[i]=='>')
+        y--;
+        else if(directions[i]=='^')
+        x--;
+        else if(directions[i]=='V')
+        x++;
+
+        gmap[mid_x+x][mid_y+y]={1};
+
+    }
+
+    for(int i=0;i<msize;i++)
+    {
+        for(int j=0; j<msize ;j++)
+        if(gmap[i][j]==1)
+        total++;
+    }
+
+    cout<<total;
+
+    return 0;
+}

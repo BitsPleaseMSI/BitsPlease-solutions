722b839d2f1a09c24142004d48e0f19aef44459f
vineetmishra2451 <vineetmishra2451@gmail.com>
2019-09-20 12:53:03 +0530

    Update 9.c

--- 9.c
+++ 9.c
@@ -1,14 +1,16 @@
-#include<string.h>
 #include<stdio.h>
+#include<string.h>
 int main()
 {
     char s[100];
     int l,c,r,cr,count;
-    printf("\nEnter the Directions(<, >, ^ & v): ");
     scanf("%s",s);
     l=strlen(s);
     cr=2*l+1;
-    int a[cr][cr]={};
+    int a[cr][cr];
+    for(int i=0;i<cr;i++)
+        for(int j=0;j<cr;j++)
+            a[i][j]=0;
     c=r=(l/2)+1;
     a[r][c]=1;
     for(int k=0;k<l;k++)
@@ -23,7 +25,7 @@ int main()
             a[r][c+1]=1;
             c++;
         }    
-        else if(s[k]=='v')
+        else if(s[k]=='v'||s[k]=='V')
         {
             a[r+1][c]=1;
             r++;
@@ -41,7 +43,7 @@ int main()
            if(a[p][q]==1)
            count++;
        }    
-    printf("\nTotal No. of House where letter(s) are delivered is: %d ",count);   
+    printf("\n%d ",count);   
     
 
 }

3aa82f8ab5535d538a6be23ea1cd3e8e192fa2d1
Tushar Bali <tusharbali11@gmail.com>
2019-09-21 09:47:56 +0530

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -10,15 +10,19 @@ int main()
     int i=0,len,sum=0,pos=0,neg=0,pos2=0;
     cin>>len;
     int s[len];
-    for(int i=0;i<len ;i++)
-        cin>>s[i];
+    for(int j=0;j<len ;j++)
+        cin>>s[j];
 
-    while(i!=len-1)
+    while(i!=len)
     {
+     if(i==0)
+     while(s[i]<0)
+     i++;
+
      pos=0;
      while(s[i]>0)
      {
-        if(i==len-1)
+        if(i==len)
          break;
         pos=s[i]+pos;
         i++;
@@ -26,15 +30,15 @@ int main()
      neg=0;
      while(s[i]<0)
      {
-        if(i==len-1)
+        if(i==len)
          break;
         neg=neg+s[i];
         i++;
      }
     pos2=0;
-     while(s[i]>0)
+     while(s[i]<0)
      {
-        if(i==len-1)
+        if(i==len)
          break;
         pos2=s[i]+pos;
         i++;
@@ -42,7 +46,7 @@ int main()
 
     if(neg+pos2>0)
         sum=pos+pos2+neg+sum;
-    if(pos2>pos && sum<pos2)
+    if(pos2>sum)
         sum=pos2;
     }
     cout<<sum<<endl;

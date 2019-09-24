5c7a25273a31fa467adf22286eed1de3d8f0e0d6
Tushar Bali <tusharbali11@gmail.com>
2019-09-24 13:01:02 +0530

    Update 26.cpp

--- 26.cpp
+++ 26.cpp
@@ -9,10 +9,7 @@ int main()
     cases--;
     int n,m;
     cin>>n;
-    if(n%2==0)
-        m=2*n+1;
-    else
-        m=2*n-1;
+    m=2*n+1;
     for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=m;j++)

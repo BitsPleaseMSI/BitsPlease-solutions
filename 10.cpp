aaa4a45090b6ceb2efa03d34b21ffa3b0b5dcc5e
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2019-10-12 19:35:43 +0530

    Update 10 (#50)
    * Update 10.cpp
    
    Better solution with meaningful variable names and comments.

--- 10.cpp
+++ 10.cpp
@@ -1,84 +1,32 @@
-#include<iostream>
+# include <bits/stdc++.h>
+
 using namespace std;
-int main()
-{
-	int n, m, i, a[30], b[30], sum[30], x, l;
 
-	//initialising sum array as 0
-	for (i = 0; i < 30; i++)
-	{
-		sum[i] = 0;
-		a[i] = 0;
-		b[i] = 0;
-	}
-
-	//input array 1
+int main() {
+	int m, n;
+	// input array sizes and arrays
 	cin >> n;
-	for (i = 0; i < n; i++)
-		cin >> a[i];
-
-	//input array 2
+	int arr1[n];
+	for (int i = n - 1; i >= 0; --i)
+		cin >> arr1[i];
 	cin >> m;
-	for (i = 0; i < m; i++)
-		cin >> b[i];
-
-	//array resizing
-	if (n > m)
-	{
-		l = n - m;
-		for (i = m; i >= 0; i--)
-			b[i + l] = b[i];
-		for (i = 0; i < l; i++)
-			b[i] = 0;
+	int arr2[m];
+	for (int i = m - 1; i >= 0; --i)
+		cin >> arr2[i];
+	int max_result_size = max(m, n); // maximum size the result array can be
+	int result[max_result_size + 1];
+	for (int i = 0; i < max_result_size + 1; ++i)
+		result[i] = 0; // setting all elements to 0
+	for (int i = 0; i < max_result_size; ++i) {
+		result[i] += arr1[i] + arr2[i];
+		if (result[i] >= 10)
+			result[i + 1]  += result[i] / 10; // carrying forward the tenths place
+		result[i] = result[i] % 10; // retaining the units place
 	}
-
-	if (m > n)
-	{
-		l = m - n;
-		for (i = n; i >= 0; i--)
-			a[i + l] = a[i];
-		for (i = 0; i < l; i++)
-			a[i] = 0;
-	}
-
-	x = n > m ? n : m;
-
-	//adding the array
-	for (i = x; i >= 0; i--)
-	{
-	//case 1: sum<10
-		if (a[i] + b[i] < 10)
-			sum[i] += a[i] + b[i];
-	//case 2: sum>=10, i!=0
-		else if (a[i] + b[i] >= 10)
-		{
-			sum[i - 1] += 1;
-			sum[i] += (a[i] + b[i]) - 10;
-		}
-	//case 3: sum>=10, i=0
-		if (i == 0 && (a[i] + b[i] >= 10))
-		{
-			for (i = x; i >= 0; i--)
-				sum[i + 1] = sum[i];
-			sum[0] = 1;
-			x += 1;
-		}
-	}
-
-	//display
-	for (i = 0; i < x; i++)
-		cout << sum[i];
-
+	int result_idx;  // to find index from where the number starts in result array
+	for (result_idx = max_result_size; result[result_idx] == 0; --result_idx);
+	// printing result
+	for (int i = result_idx; i >= 0; --i)
+		cout << result[i];
 	return 0;
 }
-
-
-
-    
-    
-    
-    
-    
-    
-    
-    

69f85c46e8d7ff67295487f9756a3c148fc7eebd
MaDDogx <animesh00914902017@msi-ggsip.org>
2019-10-12 19:33:02 +0530

    Update 10.cpp
    Refactor 10.cpp.

--- 10.cpp
+++ 10.cpp
@@ -2,28 +2,31 @@
 
 using namespace std;
 
-int main(){
-	int m, n, i;
-	cin>>n;
-	int ar1[n];
+int main() {
+	int m, n;
+	// input array sizes and arrays
+	cin >> n;
+	int arr1[n];
 	for (int i = n - 1; i >= 0; --i)
-		cin>>ar1[i];
-	cin>>m;
-	int ar2[m];
+		cin >> arr1[i];
+	cin >> m;
+	int arr2[m];
 	for (int i = m - 1; i >= 0; --i)
-		cin>>ar2[i];
-	int maxm = max(m, n); //maximum size the result array can be
-	int result[maxm + 1];
-	for (i = 0; i < maxm + 1; i++)
-		result[i] = 0; //making all elements 0
-	for (i = 0; i < maxm; ++i){
-		result[i] += ar1[i] + ar2[i];
+		cin >> arr2[i];
+	int max_result_size = max(m, n); // maximum size the result array can be
+	int result[max_result_size + 1];
+	for (int i = 0; i < max_result_size + 1; ++i)
+		result[i] = 0; // setting all elements to 0
+	for (int i = 0; i < max_result_size; ++i) {
+		result[i] += arr1[i] + arr2[i];
 		if (result[i] >= 10)
-			result[i + 1]  += result[i]/10; //carrying forward the tenths place
-			result[i] = result[i] % 10; //retaining the units place
+			result[i + 1]  += result[i] / 10; // carrying forward the tenths place
+		result[i] = result[i] % 10; // retaining the units place
 	}
-	for (i = maxm; result[i] == 0; --i); //To find index from where the number starts in result array
-	for (; i >= 0; --i)
-		cout<<result[i]; //printing result
+	int result_idx;  // to find index from where the number starts in result array
+	for (result_idx = max_result_size; result[result_idx] == 0; --result_idx);
+	// printing result
+	for (int i = result_idx; i >= 0; --i)
+		cout << result[i];
 	return 0;
 }

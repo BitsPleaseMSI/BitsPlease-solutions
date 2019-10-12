6d27ef6185f5394b16651afff3a2ddd178053b9f
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2019-10-12 19:16:43 +0530

    Update 10.cpp

--- 10.cpp
+++ 10.cpp
@@ -1,29 +1,28 @@
-# include <iostream>
+# include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
-	int n, m, i;
-	int ar1[10], ar2[10], result[10];
-	for (i = 0; i < 10; ++i)
-		ar1[i] = ar2[i] = result[i] = 0; //sets all elements of array to 0
-
+	int m, n, i;
 	cin>>n;
-	for (i = n - 1; i >= 0; --i)
-		cin>>ar1[i]; //input first array
+	int ar1[n];
+	for (int i = n - 1; i >= 0; --i)
+		cin>>ar1[i];
 	cin>>m;
-	for (i = m - 1; i >= 0; --i)
-		cin>>ar2[i]; //input second array
-
-	for (i = 0; i < 10; ++i){
+	int ar2[m];
+	for (int i = m - 1; i >= 0; --i)
+		cin>>ar2[i];
+	int maxm = max(m, n); //maximum size the result array can be
+	int result[maxm + 1];
+	for (i = 0; i < maxm + 1; i++)
+		result[i] = 0; //making all elements 0
+	for (i = 0; i < maxm; ++i){
 		result[i] += ar1[i] + ar2[i];
 		if (result[i] >= 10)
 			result[i + 1]  += result[i]/10; //carrying forward the tenths place
 			result[i] = result[i] % 10; //retaining the units place
 	}
-
-	for (i = 9; result[i] == 0; --i); //To find index from where the number starts in result array
-
+	for (i = maxm; result[i] == 0; --i); //To find index from where the number starts in result array
 	for (; i >= 0; --i)
 		cout<<result[i]; //printing result
 	return 0;

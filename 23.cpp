de97a8ad4690d3806bff205c914dab8b89de5e3b
Tushar Bali <tusharbali11@gmail.com>
2019-10-24 15:42:30 +0530

    Update 23.cpp

--- 23.cpp
+++ 23.cpp
@@ -2,28 +2,21 @@
 using namespace std;
 int main()
 {
-int cases,cases1;
-cin>>cases;
-cases1=cases;
-int ar[cases];
-
-while(cases1!=0)
+int cases;
+cin>>cases;             //to get no of cases
+while(cases--)
 {
     string input_string;
-    cin>>input_string;
-    int leng=input_string.length();
-    int i=0,flag=0;
+    cin>>input_string;          //to store no of input string
+    int leng=input_string.length();     
+    int i=0,pair=0;
         while(i!=leng-1)
         {
-            if(input_string[i]=='(' && input_string[i+1]==')')
-                flag+=2;
+            if(input_string[i]=='(' && input_string[i+1]==')')  //to get total number of complete pair
+                pair+=2;
             i++;
         }
-    ar[cases-cases1]=leng-flag;
-    cases1--;
+    cout<<leng-pair;    //difference between total length and complete pairs
 }
-
-for(int i=0;i<cases;i++)
-    cout<<ar[i]<<endl;
 return 0;
 }

6485985088384dd3a894eaa6075ba0d1b51ab311
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2020-10-07 18:32:44 +0530

    Update 16.cpp

--- 16.cpp
+++ 16.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 int main() {
     char str[100];
-    int N, n[3] = {0, 0, 0};
+    int N, n[3] = {0, 0, 0}; // To keep track of how many parentheses have been opened/closed
     cin>>N;
     for (int a = 0; a < N; ++a) {
         cin>>str;
         for (int i = 0; str[i] != '\0'; ++i) {
             switch (str[i]) {
-                case '{':   n[0] += 1;
+                case '{':   n[0] += 1; // Parentheses open
                             break;
                 case '(':   n[1] += 1;
                             break;
                 case '[':   n[2] += 1;
                             break;
-                case '}':   n[0] -= 1;
+                case '}':   n[0] -= 1; //Parentheses close
                             break;
                 case ')':   n[1] -= 1;
                             break;
@@ -24,7 +24,7 @@ int main() {
                             break;
             }
         }
-        if (n[0] == 0 && n[1] == 0 && n[2] == 0)
+        if (n[0] == 0 && n[1] == 0 && n[2] == 0) // Check whether parentheses are valid
             cout<<"YES"<<endl;
         else
             cout<<"NO"<<endl;
